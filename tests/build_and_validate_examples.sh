3ab885e83a18ddd4ddd7f9c29d61c3ce7563295f
eslof <eslof.github@gmail.com>
2023-06-01 10:47:58 +0200

    added composer test command and made bash script path safe (execute from anywhere)

--- tests/build_and_validate_examples.sh
+++ tests/build_and_validate_examples.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Loop through all .php files from the examples directory, excluding those in vendor and node_modules directories
-for file in $(find ../examples -name 'vendor' -prune -o -name 'node_modules' -prune -o -name '*.php' -print); do
+for file in $(find ${BASH_SOURCE%/*}/../examples -name 'vendor' -prune -o -name 'node_modules' -prune -o -name '*.php' -print); do
     # Remove old yaml:
     rm -f "${file%.php}.yml"
     # Build the yaml:
     php "$file"
     # Validate the yaml:
-    node "./validate.js" "${file%.php}.yml"
+    node "${BASH_SOURCE%/*}/validate.js" "${file%.php}.yml"
 done

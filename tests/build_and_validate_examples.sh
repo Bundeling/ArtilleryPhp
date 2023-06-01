5bd543b7f741dc8bc8aa2c7bce48f1d4a89e0aea
eslof <eslof.github@gmail.com>
2023-06-01 14:00:27 +0200

    more safe build and validate sh, now makes sure dependencies are installed

--- tests/build_and_validate_examples.sh
+++ tests/build_and_validate_examples.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Install test dependencies
+echo "ℹ️ Installing test dependencies..."
+cd ${BASH_SOURCE%/*} && npm install
+
+# Install examples dependencies
+echo "ℹ️ Installing examples dependencies..."
+cd ../examples && ../composer.phar install
 
 # Loop through all .php files from the examples directory, excluding those in vendor and node_modules directories
-for file in $(find ${BASH_SOURCE%/*}/../examples -name 'vendor' -prune -o -name 'node_modules' -prune -o -name '*.php' -print); do
+for file in $(find . -name 'vendor' -prune -o -name 'node_modules' -prune -o -name '*.php' -print); do
 
     # Remove old yaml:
     rm -f "${file%.php}.yml"
@@ -10,6 +17,6 @@ for file in $(find ${BASH_SOURCE%/*}/../examples -name 'vendor' -prune -o -name 
     php "$file"
 
     # Validate the yaml:
-    node "${BASH_SOURCE%/*}/validate.js" "${file%.php}.yml"
+    node "../tests/validate.js" "${file%.php}.yml"
 
 done
